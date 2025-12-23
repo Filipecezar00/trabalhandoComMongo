87a2df16b90d991439fd80cfbefb0f376ca787b3
Filipe Cezar <cezarfilipe01@gmail.com>
2025-12-23 10:57:50 -0300

    corrigindo

--- blogPasta/app.js
+++ blogPasta/app.js
@@ -1,19 +1,21 @@
 // Carregando modulos
 const express = require("express");
-const handlebars = require("express-handlebars");
-const bodyparser = require("body-parser");
-const moongose = require('mongoose');
+const {engine} = require("express-handlebars");
+// const bodyparser = require("body-parser");
+const mongoose = require('mongoose');
 const app = express();
 // configurações 
     // body parser
-    app.use(bodyparser.urlencoded({extended:true}));
-    app.use(bodyparser.json());
+    app.use(express.urlencoded({extended:true}));
+    app.use(express.json());
     //HandleBars 
-    app.engine("handlebars",handlebars({defaultLayout:'main'}));
-    app.set("view engine","handlebars");
-// rotas
+     app.engine("handlebars",engine({defaultLayout:"main"})); 
+// Mongoose 
+    // Em breve 
 
-// outros 
+//Rotas 
+
+// Outros 
 const port = 8081 ;
 app.listen(port,()=>{
 console.log("Servidor Rodando....");
