c4c44e5b4612e659898d29b41bc38dfb99b780bd
Filipe Cezar <cezarfilipe01@gmail.com>
2025-12-26 18:27:26 -0300

    atualizando

--- blogPasta/models/Postagem.js
+++ blogPasta/models/Postagem.js
@@ -4,6 +4,28 @@ const Schema = mongoose.Schema;
 const Postagem = new Schema({
     titulo:{
         type:String, 
-        require:true 
+        required:true 
     }, 
+    slug: {
+        type:String, 
+        required: true
+    }, 
+    descricao:{
+        type:String, 
+        required:true 
+    }, 
+    conteudo:{
+        type:String, 
+        required:true 
+    }, 
+    categoria:{
+        type:Schema.Types.ObjectId, 
+        ref:"categorias", 
+        required:true
+    }, 
+    data:{
+        type:Date, 
+        default:Date.now()
+    }
 })
+mongoose.model("postagens",Postagem) 
