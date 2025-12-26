0c26585230bd8199f6d68c76ae38f48bdc2bc2d5
Filipe Cezar <cezarfilipe01@gmail.com>
2025-12-26 15:29:06 -0300

    atualizando

--- blogPasta/routes/admin.js
+++ blogPasta/routes/admin.js
@@ -99,15 +99,16 @@ router.post("/categorias/nova",(req,res)=>{
       })
  })
   router.post("/categorias/deletar",(req,res)=>{
-    Categoria.remove({_id:req.body.id}).then(()=>{
+     Categoria.findByIdAndDelete(req.body.id)
+      .then(()=>{
         req.flash("success_msg","Categoria Deletada com Sucesso"); 
-        req.redirect("/admin/categorias")
-    }).catch((err)=>{
+        res.redirect("/admin/categorias")
+    })
+    .catch((err)=>{
         req.flash("error_msg","Houve um Erro ao Deletar")
-        req.redirect("/admin/categorias") 
+        res.redirect("/admin/categorias") 
     })
 }) 
-   
   router.get("/categorias/add",(req,res)=>{
   res.render("admin/addcategorias")
 })
