d8aa9b441dcc2e58dcbf2033916f38081fe3c5b7
Filipe Cezar <cezarfilipe01@gmail.com>
2025-12-30 01:00:19 -0300

    atualizando

--- blogPasta/routes/usuario.js
+++ blogPasta/routes/usuario.js
@@ -17,7 +17,22 @@ router.post("/registro",(req,res)=>{
      if(!req.body.email || typeof req.body.email==undefined || req.body.email==null){
         erros.push({texto:"Email Inválido"}); 
     }
-    
+     if(!req.body.senha || typeof req.body.senha==undefined || req.body.senha==null){
+        erros.push({texto:"Senha Inválida"}); 
+    }
+     if(req.body.senha.length<4){
+        erros.push({texto:"Senha Muito Curta"}); 
+    }
+    if(req.body.senha != req.body.senha2){
+        erros.push({texto:"Senha Diferente da Anterior"}); 
+    }
+
+    if(erros.length>0){
+        res.render("usuarios/registro",{erros:erros})
+    }else{
+
+    }
+
 })
 
 module.exports = router
