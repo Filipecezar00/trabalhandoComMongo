d69473ede92321fd4ba2fcd54c63e495be9d4640
Filipe Cezar <cezarfilipe01@gmail.com>
2025-12-30 00:51:31 -0300

    atualizando

--- blogPasta/routes/usuario.js
+++ blogPasta/routes/usuario.js
@@ -9,4 +9,15 @@ router.get("/registro",(req,res)=>{
     res.render('usuarios/registro')
 })
 
+router.post("/registro",(req,res)=>{
+    var erros=[]
+    if(!req.body.nome || typeof req.body.nome==undefined || req.body.nome==null){
+        erros.push({texto:"Nome Inválido"}); 
+    }
+     if(!req.body.email || typeof req.body.email==undefined || req.body.email==null){
+        erros.push({texto:"Email Inválido"}); 
+    }
+    
+})
+
 module.exports = router
